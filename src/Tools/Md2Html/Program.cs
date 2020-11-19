bf4a74b2181c48ded3a13ed4b3632201090fd242
Jorgen Dahl <jorgen.dahl@autodesk.com>
2020-11-19 16:08:19 -0500

    Add ndesk support and implement help option

--- src/Tools/Md2Html/Program.cs
+++ src/Tools/Md2Html/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using NDesk.Options;
 
 namespace Md2Html
 {
@@ -7,20 +8,26 @@ namespace Md2Html
     {
         static void Main(string[] args)
         {
+            if (CheckForHelp(args))
+            {
+                // Display help and exit
+                return;
+            }
+
             Console.WriteLine("");
 
             while (true)
             {
                 var line = Console.ReadLine();
-                if (line == "<<<<<Sanitize>>>>>")
+                if (line == @"<<<<<Sanitize>>>>>")
                 {
                     Sanitize();
                 }
-                else if (line == "<<<<<Convert>>>>>")
+                else if (line == @"<<<<<Convert>>>>>")
                 {
                     Convert();
                 }
-                Console.WriteLine("<<<<<Eod>>>>>");
+                Console.WriteLine(@"<<<<<Eod>>>>>");
             }
         }
 
@@ -53,12 +60,60 @@ namespace Md2Html
             while (true)
             {
                 var line = Console.ReadLine();
-                if (line == "<<<<<Eod>>>>>")
+                if (line == @"<<<<<Eod>>>>>")
                 {
                     break;
                 }
                 data.WriteLine(line);
             }
+        }
+
+        static bool CheckForHelp(string[] args)
+        {
+            bool help = false;
+            var options = new OptionSet()
+            {
+                {@"h|?|help", @"Show help and exit", v => help = v != null },
+            };
+
+            options.Parse(args);
+            if (help)
+            {
+                options.WriteOptionDescriptions(Console.Out);
+                DisplayHelp();
+            }
+
+            return help;
+        }
+        static void DisplayHelp()
+        {
+            Console.WriteLine(@"");
+            Console.WriteLine(@"This tool converts Markdown to Html or Sanitize html");
+            Console.WriteLine(@"and reads from stdin and outputs to stdout");
+            Console.WriteLine(@"");
+            Console.WriteLine(@"Converting Markdown to Html");
+            Console.WriteLine(@"---------------------------");
+            Console.WriteLine(@"Format markdown as follows:");
+            Console.WriteLine(@"<<<<<Convert>>>>>");
+            Console.WriteLine(@"Markdown formatted data here");
+            Console.WriteLine(@"<<<<<Eod>>>>>");
+            Console.WriteLine(@"");
+            Console.WriteLine(@"Output will be:");
+            Console.WriteLine(@"Html formatted data");
+            Console.WriteLine(@"<<<<<Eod>>>>>");
+            Console.WriteLine(@"");
+            Console.WriteLine(@"Sanitize Html");
+            Console.WriteLine(@"-------------");
+            Console.WriteLine(@"Format html data as follows:");
+            Console.WriteLine(@"<<<<<Sanitize>>>>>");
+            Console.WriteLine(@"Html data to sanitize here");
+            Console.WriteLine(@"<<<<<Eod>>>>>");
+            Console.WriteLine(@"");
+            Console.WriteLine(@"Output will be:");
+            Console.WriteLine(@"Sanitized Html data");
+            Console.WriteLine(@"<<<<<Eod>>>>>");
+            Console.WriteLine(@"");
+            Console.WriteLine(@"");
         }
     }
 }
