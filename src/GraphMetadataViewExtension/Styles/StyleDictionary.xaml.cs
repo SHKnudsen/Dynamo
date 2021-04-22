0ba3ae3d068a5a04cc5fc9258407bae1e599b0ee
Martin Stacey <martin__stacey@hotmail.com>
2021-04-22 10:55:08 +0100

    Fix styling to use dynamo colors and hover effects on graph side bar, this works with local references

---
+++ src/GraphMetadataViewExtension/Styles/StyleDictionary.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dynamo.GraphMetadata.Styles
+{
+    /// <summary>
+    /// Interaction logic for StyleDictionary.xaml
+    /// </summary>
+    public partial class StyleDictionary : ResourceDictionary
+    {
+        public StyleDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
